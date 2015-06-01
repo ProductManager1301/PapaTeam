69ba6da1ec9da027266ce12468baf4b58f573ba5
ZhongWeidi <git>
2015-06-01 17:19:30 +0800

    追风小组
    版本一

---
+++ PaPaTeam/PaPaTeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaPaTeam
+//
+//  Created by 钟伟迪 on 15/6/1.
+//  Copyright (c) 2015年 追风小组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaPaTeam/PaPaTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaPaTeam
+//
+//  Created by 钟伟迪 on 15/6/1.
+//  Copyright (c) 2015年 追风小组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
